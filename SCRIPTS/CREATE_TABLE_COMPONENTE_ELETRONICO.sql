21e16b66ef3a0257c0e292c7d1d9c6db870425f2
Antonio Filho <antoniofilhonerde@gmail.com>
2018-09-05 14:50:17 -0300

    backup

--- SCRIPTS/CREATE_TABLE_COMPONENTE_ELETRONICO.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE Componente_Eletronico(
-	NumeroSerie VARCHAR(15) NOT NULL UNIQUE,
-	Tipo VARCHAR(20) NOT NULL,
-	ValorUnitario FLOAT(2) NOT NULL,
-	Funcao VARCHAR(30) NOT NULL,
-	Especificacao VARCHAR(50) NOT NULL,
-	CONSTRAINT PK_Componente PRIMARY KEY (NumeroSerie)
-);
